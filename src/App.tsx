6c65f17c3232404a48b99b60a3360ee5a066957b
user-iana <ianamldv@gmail.com>
2023-10-30 18:15:19 +0200

    Developed Navigation Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { NavLinkProps, navLinks } from "./components/links/navbar-routing";
-import './App.css';
-import NavigationBar from "./navBar";
+import NavigationBar from './components/NavigationBar';
 
-
-import About from './components/about';
-import Books from './components/books';
-import Profile from './components/profile';
-import Movies from './components/movies';
-import SingIn from './components/sign-in';
-import LogOut from './components/log-out';
 
 const App: React.FC = () => {
   return (
@@ -23,12 +15,10 @@ const App: React.FC = () => {
   );
 };
 
+
 function getComponent(props: NavLinkProps) {
   const { path, component, title } = props;
   return <Route path={path} Component={component} key={title} />;
 }
 
 export default App;
-
-function AccountMenu () {
-}
