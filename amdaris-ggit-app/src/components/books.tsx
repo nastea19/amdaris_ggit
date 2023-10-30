3ee1e7c19a1866df87881b188f81f33380e76d00
nastea19 <nastea.popova1907@gmail.com>
2023-10-30 22:26:02 +0200

    additional commit

--- amdaris-ggit-app/src/components/books.tsx
+++ amdaris-ggit-app/src/components/books.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import '../App.css';
 
-export function Books() {
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import Input from './input';
+
+export default function Books() {
+  const [value, setValue] = React.useState("one");
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+
+  };
+
   return (
-    <div className="App">
-      Book Component
-    </div>
+    <Box sx={{ width: "100%" }}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        textColor="secondary"
+        indicatorColor="secondary"
+        aria-label="secondary tabs example"
+      >
+        <Tab value="one" label="In progress" />
+        <Tab value="two" label="Finished" />
+        <Tab value="three" label="In plan" />
+      </Tabs>
+    </Box>
   );
 }
-
-export default Books;
