c722a97d00b13c10b5c03104c0c10162f67ebb10
nastea19 <nastea.popova1907@gmail.com>
2023-10-31 19:33:53 +0200

    interface Book

--- amdaris-ggit-app/src/components/books.tsx
+++ amdaris-ggit-app/src/components/books.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
-import '../App.css';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+// import Input from './input';
 
-export function Books() {
-  return (
-    <div className="App">
-      Book Component
-    </div>
+// interface Book with fields(id, name, author …
+interface Book {
+  id: number;
+  name: string;
+  author: string;
+  language: string;
+}
+
+
+export default function Books() {
+  const [value, setValue] = React.useState("one");
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
+return (
+    <Box sx={{ width: "100%" }}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        textColor="secondary"
+        indicatorColor="secondary"
+        aria-label="secondary tabs example"
+      >
+        <Tab value="one" label="In progress" />
+        <Tab value="two" label="Finished" />
+        <Tab value="three" label="In plan" />
+      </Tabs>
+    </Box>
   );
 }
 
-export default Books;
+
