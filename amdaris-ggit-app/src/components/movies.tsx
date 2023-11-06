6d10a7fa460afece407dd5929327e8d43408d6b2
user-iana <ianamldv@gmail.com>
2023-11-06 15:52:51 +0200

    changes in movies.tsx and movie-table.tsx

--- amdaris-ggit-app/src/components/movies.tsx
+++ amdaris-ggit-app/src/components/movies.tsx
@@ -2,26 +2,43 @@ import React from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
+import { addMovie, getMovies } from "../services/movie-service";
+import MovieTable from "./movie-table";
 
-export interface Film {
+export interface Movie {
   id: number;
   title: string;
-  director: string;
-  releaseYear: number;
   genre: string;
-  rating: number;
-  durationMinutes: number;
+  length: string;
+  country: string;
+  watchStatus: string;
 }
 
 export default function Movies() {
-  const [value, setValue] = React.useState("one");
+  const [value, setValue] = React.useState("watching");
+  const movies = getMovies();
+  const [moviesLength, setLength] = React.useState(movies.length);
+  const watchedMovies = movies.filter((x) => x.watchStatus === "watched");
+  const toWatchMovies = movies.filter((x) => x.watchStatus === "to watch");
+  const watchingMovies = movies.filter((x) => x.watchStatus === "watching");
+
+  const [rowMovies, setMovies ] = React.useState<Movie[]>(watchingMovies);
+
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    if (newValue === "to watch") {
+      setMovies(toWatchMovies);
+    } else if (newValue === "watched") {
+      setMovies(watchedMovies);
+    } else if (newValue === "watching") {
+      setMovies(watchingMovies);
+    }
+
     setValue(newValue);
   };
 
   return (
-    <Box sx={{ width: "100%" }}>
+    <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center"  }}>
       <Tabs
         value={value}
         onChange={handleChange}
@@ -29,10 +46,21 @@ export default function Movies() {
         indicatorColor="secondary"
         aria-label="secondary tabs example"
       >
-        <Tab value="one" label="I want to Watch" />
-        <Tab value="two" label="I'm watching" />
-        <Tab value="three" label="I watched" />
+        <Tab value="to watch" label="I want to Watch" />
+        <Tab value="watching" label="I'm watching" />
+        <Tab value="watched" label="I watched" />
       </Tabs>
+
+      <MovieTable
+        movies={rowMovies}
+        onAddMovie={(newMovie: Movie) => {
+          watchingMovies.push(newMovie);
+          setLength(moviesLength + 1);
+          addMovie(newMovie);
+        }}
+        moviesLength={moviesLength}
+      />
+
     </Box>
   );
 }
