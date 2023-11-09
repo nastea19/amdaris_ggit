e53e5a16620ead18654de61b46bda65edf1ccb1c
Ana Ceban <ana.ceban@amdaris.com>
2023-11-09 22:23:44 +0200

    Display pictures

--- amdaris-ggit-app/src/components/book-card.tsx
+++ amdaris-ggit-app/src/components/book-card.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 interface BookCardProps {
   title: string;
@@ -23,7 +23,7 @@ const BookCard: React.FC<BookCardProps> = ({
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardHeader title={title} subheader={subheader} />
-      <CardMedia component="img" height="194" image={image} alt={alt} />
+      <CardMedia component="img" image={image} alt={alt} />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
           {description}
