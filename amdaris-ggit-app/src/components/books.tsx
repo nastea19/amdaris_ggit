55e838ace9a51d5b3394692bea2970ed927ed63f
nastea19 <nastea.popova1907@gmail.com>
2023-11-09 16:32:17 +0200

    add new books card

--- amdaris-ggit-app/src/components/books.tsx
+++ amdaris-ggit-app/src/components/books.tsx
@@ -5,6 +5,8 @@ import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
 import BookTable from "./book-table";
 import { addBook, getBooks, updateBook } from "../services/book-service";
+import BookCard from "./book-card";
+import rwrb from "/images/rwrb.jpeg";
 
 export interface Book {
   id: number;
@@ -37,14 +39,29 @@ export default function Books() {
   };
 
   return (
-    <Box
-      sx={{ 
-        display: "flex", 
-        flexDirection: "column", 
-        alignItems: "center",
-        fontFamily: "Libre Baskerville, serif",
-      }}
-    >
+    <Box>
+      <h2 className="h2">Trending romance books:</h2>
+      <div className="bookcard">
+        <div className="bookcardspacing">
+          <BookCard
+            title="It Ends with Us"
+            subheader="Colleen Hoover, 2016"
+            image="amdaris-ggit-app/src/images/itendswithus.jpeg"
+            alt="It Ends with Us"
+            description="Lily hasn't always had it easy, but that's never stopped her from working hard for the life she wants. She's come a long way from the small town in Maine where she grew up — she graduated from college, moved to Boston, and started her own business. So when she feels a spark with a gorgeous neurosurgeon named Ryle Kincaid, everything in Lily's life suddenly seems almost too good to be true."
+          />
+        </div>
+
+        <div className="bookcardspacing">
+          <BookCard
+            title="Red, White & Royal Blue"
+            subheader="Casey McQuisto, 2019"
+            image="/images/rwrb.jpeg"
+            alt="Red, White & Royal Blue"
+            description="First Son Alex Claremont-Diaz is the closest thing to a prince this side of the Atlantic. With his intrepid sister and the Veep's genius granddaughter, they're the White House Trio, a beautiful millennial marketing strategy for his mother, President Ellen Claremont. International socialite duties do have downsides—namely, when photos of a confrontation with his longtime nemesis Prince Henry at a royal wedding leak to the tabloids and threaten American/British relations. The plan for damage control: staging a fake friendship between the First Son and the Prince."
+          />
+        </div>
+    </div>
       <Tabs
         value={value}
         onChange={handleChange}
@@ -74,6 +91,7 @@ export default function Books() {
           updateBook(bookId, book);
         }}
       />
+      
     </Box>
   );
 }
